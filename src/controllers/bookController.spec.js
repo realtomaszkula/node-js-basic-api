afdc70880b26248fed1801b74979e8283539616c
Tomasz Kula <tomaszkula07@gmail.com>
2017-05-27 17:10:21 +0200

    Add jasmine with reporter

---
+++ src/controllers/bookController.spec.js
@@ -0,0 +1,14 @@
+import getBookController from './bookController';
+
+
+describe('BookController', () => {
+
+  describe('POST', () => {
+
+    it('should fail for book witout title', () => {
+      expect(false).toBeFalsy();
+    });
+
+  });
+
+});
